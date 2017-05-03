27f953077dde4a61676501d6781bf432eba3d460
michaelwishlow <michael@pantheritconsulting.com>
2017-05-03 16:19:23 -0400

    complete main.js

--- js/main.js
+++ js/main.js
@@ -12,6 +12,21 @@ function getMovies (searchText){
 	axios.get('http://www.omdbapi.com?s='+searchText)    //call to the IMDB-like API
 			.then((response) => {
 				console.log(response);
+				let movies = response.data.Search;
+				let output = '';
+				$.each(movies, (index, movie) => {
+					output += `
+						<div class="col-md-3">
+							<div class="well text-center">
+								<img src="${movie.Poster}">
+								<h5>${movie.Title}</h5>
+								<a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+							</div>
+						</div>
+					`;
+				});
+
+				$('#movies').html(output);
 			})
 			.catch((err) => {
 				console.log(err);
